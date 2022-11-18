8cda8ae299869a6e286688e940074b0c74ac6745
Diogo Leite <diogoleite87@gmail.com>
2022-11-18 17:25:50 -0300

    feat: template screen mesa

---
+++ mobile/src/screens/Mesa/inde.tsx
@@ -0,0 +1,20 @@
+import { Input } from "../../components/Input";
+import { Container, ContentBody, ContentFooter, ContentHeader } from "./styles";
+
+const Mesa: React.FC = () => {
+    return (
+        <Container>
+            <ContentHeader>
+
+            </ContentHeader>
+            <ContentBody>
+
+            </ContentBody>
+            <ContentFooter>
+
+            </ContentFooter>
+        </Container>
+    )
+}
+
+export { Mesa }
---
+++ mobile/src/screens/Mesa/styles.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
+import { getStatusBarHeight } from "react-native-status-bar-height";
+
+export const Container = styled.View`
+  width: 100%;
+  height: 100%;
+  flex: 1;
+  padding: ${RFValue(16)}px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUNDLIGHT};
+`
+export const ContentHeader = styled.View`
+  margin-top: ${RFValue(getStatusBarHeight(true))}px;
+  width: 100%;
+`
+export const ContentBody = styled.View`
+  width: 100%;
+  margin-bottom: ${RFValue(20)}px;
+  align-items: center;
+`
+export const ContentFooter = styled.View`
+  width: 100%;
+  margin-bottom: ${RFValue(20)}px;
+  flex-direction: column;
+  align-items: center;
+`
+export const Title = styled.Text`
+  font-size: ${RFValue(20)}px;
+`
