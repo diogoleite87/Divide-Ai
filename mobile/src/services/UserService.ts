fb7b63a360179c53134846b5a35ff30ca37b14c2
Diogo Leite <diogoleite87@gmail.com>
2022-11-17 23:18:08 -0300

    feat: add create user request

--- mobile/src/services/UserService.ts
+++ mobile/src/services/UserService.ts
@@ -1,8 +1,11 @@
 import Api from "../providers";
+import { CreateUserDTO } from "../schemas/DTO";
 import { Profile } from "../schemas/Models";
 
 const getProfile = () => Api.get<Profile>(`/profile`)
+const createUser = (user: CreateUserDTO) => Api.post<void>('/user', user)
 
 export const UserService = {
-    getProfile
+    getProfile,
+    createUser
 }
