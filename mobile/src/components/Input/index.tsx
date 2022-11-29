6869d906b88ea1e7640e8d8ba66178138b95cefc
Diogo Leite <diogoleite87@gmail.com>
2022-11-28 23:04:45 -0300

    update: add disable boolean and value in input

--- mobile/src/components/Input/index.tsx
+++ mobile/src/components/Input/index.tsx
@@ -4,10 +4,12 @@ import { Container, TextInput } from './styles'
 
 interface propsInput {
     icon?: ReactElement
+    value?: string
+    disable?: boolean
     placeholder: string
     secureTextEntry?: boolean
     inputRef?: any
-    returnKeyType: 'done' | 'go' | 'next' | 'search' | 'send'
+    returnKeyType?: 'done' | 'go' | 'next' | 'search' | 'send'
     keyboardType?: 'default' | 'email-address' | 'numeric' | 'phone-pad' | 'number-pad' | 'decimal-pad'
     onChangeText: (text: string) => void
     onSubmitEditing?: () => void
@@ -16,6 +18,8 @@ interface propsInput {
 
 const Input: React.FC<propsInput> = ({
     icon,
+    disable,
+    value,
     placeholder,
     inputRef,
     secureTextEntry,
@@ -37,6 +41,8 @@ const Input: React.FC<propsInput> = ({
                 onChangeText={onChangeText}
                 autoCorrect={false}
                 autoCapitalize='none'
+                editable={disable}
+                defaultValue={value}
             />
         </Container>
     )
